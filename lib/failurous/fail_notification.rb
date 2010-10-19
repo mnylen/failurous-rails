c32322fbac7769be38d5853b6360c50317921332
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-19 18:02:08 +0300

    Removed the old code and documented the intended usage.

--- lib/failurous/fail_notification.rb
+++ lib/failurous/fail_notification.rb
@@ -1,62 +1,54 @@
 require 'active_support'
 
 module Failurous
+  # Notification of a fail with all the details. Can be sent to the Failurous
+  # after building.
+  #
+  # Usage:
+  #
+  #    FailNotification.build do |notification|
+  #      notification.title "Title to show in Failurous"
+  #      notification.location "something indicating the location"
+  #      notification.use_title_in_checksum false
+  #      notification.use_location_in_checksum true
+  #      
+  #      notification.section(:summary) do |summary|
+  #        summary.field(:type, "NoMethodError", {:use_in_checksum => true})
+  #        summary.field(:message, "Called `tap' for nil:NilClass", {:use_in_checksum => true})
+  #      end
+  #
+  #      notification.section(:request) do |request|
+  #        request.field(:HTTP_USER_AGENT, "Mozilla ...", {:humanize_field_name => false})
+  #      end
+  #    end
+  #
+  # To use an exception as a basis for the notification, pass the exception as a parameter to
+  # `build' method. See the documentation for `build' for information on the sections and fields
+  # added by default. You can add more sections and fields in the block.
+  #
+  #    FailNotification.build(exception) do |notification|
+  #      notification.section(:your_app_name) { |my_app| my_app.field(:username, "...") }
+  #    end
+  #
+  # To send the notification after it is built, use the `send' method (also allows for
+  # optional exception):
+  #
+  #     FailNotification.send(exception) do |notification|
+  #       ...
+  #     end
+  #
+  # Make sure the notifier is configured before using `send'!
   class FailNotification
-    attr_accessor :title
-    attr_accessor :data
-  
-    # Builds a Notification from the given exception:
-    #
-    # Title: #{type}: #{message}
-  
-    # Summary
-    #   - type
-    #   - message (not used when combining)
-    #   - topmost line in backtrace
-    # 
-    # Details:
-    #   - full backtrace
-    #
-    def from_exception(exception)
-      self.set_title("#{exception.class}: #{exception.message}").
-        add_field(:summary, :type, exception.class.to_s, {:use_in_checksum => true}).
-        add_field(:summary, :message, exception.message, {:use_in_checksum => false}).
-        add_field(:summary, :topmost_line_in_backtrace, exception.backtrace[0], {:use_in_checksum => true}).
-        add_field(:details, :full_backtrace, exception.backtrace.join('\n'), {:use_in_checksum => false})
-    end
-  
-    def set_title(title)
-      @title = title
-      self
-    end
-  
-    def add_field(section_name, field_name, field_value, field_options = {})
-      @data ||= []
+    attr_accessor :notification
     
-      found = false
-      field   = [field_name, field_value, field_options]
-    
-      data.each do |existing_section|
-        if existing_section[0] == section_name
-          existing_section[1] << field
-          found = true
-          break
-        end
+    def initialize
+      @notification = returning Dictionary.new do |defaults|
+        defaults[:title] = ""
+        defaults[:location] = ""
+        defaults[:use_title_in_checksum] = false
+        defaults[:use_location_in_checksum] = true
+        defaults[:data] = []
       end
-    
-      unless found
-        data << [section_name, [field]]
-      end
-    
-      self
-    end
-  
-    def to_json
-      ActiveSupport::JSON.encode({:title => @title, :data => @data})
-    end
-    
-    def send
-      FailNotifier.send_fail(self)
     end
   end
 end
