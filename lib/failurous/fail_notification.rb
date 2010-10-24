b2e5ee73906f58201392d3736fa1e1b7c7e82fed
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-24 10:24:57 +0300

    New syntax - documentation

--- lib/failurous/fail_notification.rb
+++ lib/failurous/fail_notification.rb
@@ -1,246 +1,187 @@
-require 'active_support'
-require 'dictionary'
-require 'rails'
-
 module Failurous
-  # Notification of a fail with all the details. Can be sent to the Failurous
-  # after building.
-  #
-  # Usage:
-  #
-  #    FailNotification.build do |notification|
-  #      notification.title "Title to show in Failurous"
-  #      notification.location "something indicating the location"
-  #      notification.use_title_in_checksum false
-  #      notification.use_location_in_checksum true
-  #      
-  #      notification.section(:summary) do |summary|
-  #        summary.field(:type, "NoMethodError", {:use_in_checksum => true})
-  #        summary.field(:message, "Called `tap' for nil:NilClass", {:use_in_checksum => true})
-  #      end
-  #
-  #      notification.section(:request) do |request|
-  #        request.field(:HTTP_USER_AGENT, "Mozilla ...", {:humanize_field_name => false})
-  #      end
-  #    end
-  #
-  # To use an exception as a basis for the notification, pass the exception as a parameter to
-  # {.build} method. See the documentation for {.build} for information on the sections and fields
-  # added by default. You can add more sections and fields in the block.
-  #
-  #    FailNotification.build(exception) do |notification|
-  #      notification.section(:your_app_name) { |my_app| my_app.field(:username, "...") }
-  #    end
-  #
-  # To send the notification after it is built, use the {.send} method (also allows for
-  # optional exception):
-  #
-  #     FailNotification.send(exception) do |notification|
-  #       ...
-  #     end
-  #
   class FailNotification
-    attr_accessor :notification_data
-    
-    def initialize
-      @notification_data = {}.tap do |defaults|
-        defaults[:title] = ""
-        defaults[:location] = ""
-        defaults[:use_title_in_checksum] = false
-        defaults[:use_location_in_checksum] = true
-        defaults[:sections] = Dictionary.new
-      end
-    end
-    
-    
-    # Creates a new FailNotification instance and passes it to the block. Optionally
-    # takes a exception instance which can be used to prepopulate the notification
-    # with the following sections and fields:
-    #
-    # * *:summary* - Summary
-    #   * *:type* - Type of exception (e.g. NoMethodError) (used in checksum)
-    #   * *:message* - Exception message (not used in checksum)
-    #   * *:top_of_backtrace* - The topmost frame in backtrace (used in checksum)
-    # 
-    # * *:details* - Details
-    #   * *:full_backtrace* - Full backtrace (not used in checksum)
-    #
-    # The title for the notification will be *type:* *message* (not used in checksum) and the location
-    # will be the topmost frame in backtrace (used in checksum)
-    def self.build(exception = nil, &block)
-      notification = FailNotification.new
-      notification.prepopulate_from_exception(exception) if exception
-      
-      if block_given?
-        block.call(notification)
-      end
-      
-      notification
-    end
-    
-    
-    def self.send(exception = nil, &block)
-      notification = self.build(exception, &block)
-      FailNotifier.send_fail(notification)
-      
-      notification
-    end
-    
-    
-    # Sets the title for the fail notification
-    def title(title)
-      @notification_data[:title] = title
-    end
-    
-    # Sets the location for the fail notification
-    def location(location)
-      @notification_data[:location] = location
-    end
-    
-    
-    # Sets the boolean flag for using title in the checksum on Failurous
-    def use_title_in_checksum(value = false)
-      @notification_data[:use_title_in_checksum] = value
-    end
-    
-    # Sets the boolean flag for using location in the checksum on Failurous 
-    def use_location_in_checksum(value = true)
-      @notification_data[:use_location_in_checksum] = value
-    end
-    
-    # Creates a new section with the given name (or returns existing one)
-    def section(name, &block)
-      section = @notification_data[:sections][name] || Section.new(name)
-      @notification_data[:sections][name] = section
-      
-      if block_given?
-        block.call(section)
-      end
-      
-      section
-    end
-    
-    # Converts the notification to internal format used by Failurous,
-    # so it can be easily encoded as JSON and sent
-    def convert_to_failurous_internal_format
-      {
-        :title => @notification_data[:title],
-        :location => @notification_data[:location],
-        :use_title_in_checksum => @notification_data[:use_title_in_checksum],
-        :use_location_in_checksum => @notification_data[:use_location_in_checksum],
-        :data => [].tap { |data| @notification_data[:sections].values.each { |section| data << section.convert_to_failurous_internal_format } }
-      }
-    end
-    
-    
-    def prepopulate_from_exception(exception)
-      backtrace = Rails.respond_to?(:backtrace_cleaner) ?
-        Rails.backtrace_cleaner.send(:filter, exception.backtrace) :
-        exception.backtrace
-      
-      self.title "#{exception.class.to_s}: #{exception.message}"
-      self.location "#{exception.backtrace[0]}"
 
-      self.section(:summary) do |summary|
-        summary.field(:type, exception.class.to_s, {:use_in_checksum => true})
-        summary.field(:message, exception.message, {:use_in_checksum => false})
-        summary.field(:top_of_backtrace, backtrace[0], {:use_in_checksum => true})
-      end
+    # Creates a new FailNotification using the specified title. The notification
+    # will be filled with details from the exception and object, if they are given.
+    #
+    # Location will default to the caller and is used when combining fails.
+    #
+    # @param title [String] custom title for the notification, if any -- when given, used when combining fails
+    # @param exception [Exception] exception that occured, if any
+    # @param object [Object] object that caused the fail notification to be built, if applicable
+    #
+    # @see FailNotification#fill_from_exception
+    # @see FailNotification#fill_from_object
+    def initialize(title = nil, exception = nil, object = nil)
 
-      self.section(:details) do |details|
-        details.field(:full_backtrace, backtrace.join('\n'), {:use_in_checksum => false})
-      end
     end
-    
-  end
-  
-  # Represents a section inside {FailNotification}.
-  #
-  # Sections are created using the {FailNotification#section} method - you should
-  # not directly create sections using {.new}
-  class Section
-    def initialize(name)
-      @fields = Dictionary.new
-      @name = name
-    end
-    
-    
-    # Creates a new field to the section. If the field is already in the section,
-    # it will be replaced with the new one.
+
+
+    # Fills notification details from the given exception. Please note that
+    # if the title and/or location has been specified previously, those
+    # are not overriden here.
     #
-    # Failurous currently supports at least the following options:
-    # * *:use_in_checksum* - use the field value in checksum
-    # * *:humanize_field_name* - humanize field name when displaying the 
-    # To see full list of field options, please consult Failurous documentation.
-    # 
-    # The builder also supports the following options for field placement:
-    # * *:before* - insert the new field before the specified field 
-    # * *:after*  - insert the new field after the specified field
+    # * *Title* will be set to _type:_ _message_. Not used when combining fails. 
+    # * *Location* will be set as the topmost frame in backtrace. Used when combining fails.
     #
-    # If *:before* and *:after* is omitted, the field will be placed after the last
-    # field in the section.
-    # 
-    # @param name     name of the field
-    # @param value    field value
-    # @param options  any options
-    def field(name, value, options = {})
-      field = Field.new(name, value, options.dup)
-      
-      if options[:after] or options[:before]
-        @fields.delete(name) if @fields.has_key?(name)
-        
-        i = 0
-        search = options[:before] || options[:after]
-        @fields.order.each do |key|
-          break if key == search
-          i += 1
-        end
-        
-        i ||= @fields.size
-        @fields.insert(i, name, field) if options[:before]
-        @fields.insert(i+1, name, field) if options[:after]
-      else
-        @fields[name] = field
-      end
-      
-      field
+    # Two sections will be added:
+    #
+    # Summary
+    # * Type (*:type*) of the exception -- used when combining fails 
+    # * Exception *message* (*:message*) -- not used when combining fails
+    # * Topmost line in backtrace (*:topmost_line_in_backtrace*) -- used when combining fails 
+    #
+    # Details
+    # * Full backtrace (*:full_backtrace*) -- not used when combining fails
+    #
+    # @param exception [Exception] the exception raised 
+    # @return [FailNotification] self
+    def fill_from_exception(exception)
+
     end
-    
-    # Converts the section to internal format used by Failurous, 
-    # so it can be easily encoded as JSON and sent
-    def convert_to_failurous_internal_format
-      data = [@name, []]
-      fields.each do |field|
-        data[1] << field.convert_to_failurous_internal_format
-      end
-      
-      data
+
+
+    # Fills notification details from the given object. Currently can be used only
+    # with derivatives of {ActionController::Base}. Filling details from controllers
+    # is delegated to {#fill_from_controller}
+    #
+    # @param object [Object] the object to fill from
+    # @see FailNotification#fill_from_controller
+    # @return [FailNotification] self
+    def fill_from_object(object)
+
     end
-    
-    
-    def fields
-      @fields.values
+
+
+    # Fills notification details from the given controller. Sets the location to
+    # _controller_name_#_action_name_, unless the location has been set by a call to
+    # {#location=}. The location will be used when combining fails.
+    #
+    # The following sections will be added:
+    #
+    # Details:
+    # * Parameters (_:parameters_) - the submitted _params_ - not used when combining fails
+    #
+    # Request: (none of these is used when combining fails, field names are not humanized)
+    #
+    # * REQUEST_METHOD
+    # * REQUEST_URI
+    # * REMOTE_ADDR
+    # * HTTP_USER_AGENT
+    #
+    # @param controller [ActionController::Base] the controller
+    # @return [FailNotification] self
+    def fill_from_controller(controller)
+
     end
-  end
-  
-  class Field
-    attr_accessor :name, :value, :options
+
     
-    
-    def initialize(*args)
-      @name, @value, @options = args
-      filter_options!
+    # Adds a field to the notification. The field will be added to the named section _section_name_ with
+    # name _field_name_ and value of _field_value_.
+    #
+    # If the section does not exists, it will be created. Otherwise the field is added to the section.
+    # If the field already exists inside the section, it will be overriden.
+    #
+    # _field_options_ can contain any options for fields supported by Failurous. At least the following
+    # are supported:
+    # * _:use_in_checksum_ - *true* if the field should be used when combining fails, defaults to *false*
+    # * _:humanize_field_name_ - *true* if the field name should be humanized, defaults to *true*
+    # For a full list of supported field options, please see the API documentation for Failurous.
+    #
+    # Placement of the field inside section can be controller with _placement_. If no placement options
+    # are given, the field will be appended to the end of the section. Supported options are:
+    # * _:below_ - the field should be placed below the specified field (e.g. *:below* => *:type*)
+    # * _:above_ - the field should be placed above the specified field (e.g. *:after* => *:message*)
+    # You can use _:first_ and _:last_ to place the field below or above the first or last field in the section.
+    # Currently combining _:below_ and _:above_ is not supported.
+    #
+    # @param section_name [Symbol] name of the section
+    # @param field_name [Symbol] name of the section
+    # @param field_value [Object] value for the field, will be converted to string using {Object.inspect} if it isn't already a string
+    # @param field_options [Hash] options for field
+    # @param placement [Hash] placement options
+    #
+    # @return [FailNotification] self
+    def add_field(section_name, field_name, field_value, field_options = {}, placement = {})
+
     end
-    
-    
-    def filter_options!
-      @options.delete(:before)
-      @options.delete(:after)
+
+
+    # Moves the named section inside the notification to either below another or
+    # above another section.
+    #
+    # _placement_ can be one of the following:
+    # * _:below_ - the section should be moved below another section (e.g. *:below* => *:summary*)
+    # * _:above_ - the section should be moved above another section (e.g. *:above* => *:request*)
+    #
+    # @param section_name [Symbol] the section to move
+    # @param placement [Hash] placement option 
+    # @return [FailNotification] self
+    def move_section(section_name, placement) 
+
     end
-    
-    
-    def convert_to_failurous_internal_format
-      [name, value, options]
+
+    # Gets the location, or the caller of {#initialize} when the location has not been
+    # set by using {#location=}
+    #
+    # @return location
+    def location
+
+    end
+
+    # Sets the location to the specified location. Will override any previous
+    # value.
+    #
+    # @param location [String] the location
+    # @see #use_location_in_checksum
+    # @see #use_location_in_checksum=
+    def location=(location)
+
+    end
+
+    # Gets whether _location_ should be used when combining fails. Defaults to *false*
+    #
+    # @return [Boolean] *true* if location should be used when combining fails; *false* otherwise
+    def use_location_in_checksum
+
+    end
+
+    # Sets whether _location_ should be used when combining fails. 
+    #
+    # @param value [Boolean] *true* if location should be used when combining fails; *false* otherwise
+    def use_location_in_checksum=(value)
+
+    end
+
+
+    # Gets the title for notification
+    #
+    # @return title 
+    def title 
+
+    end
+
+    # Sets the title to specified title. Will override any previous value.
+    #
+    # @param title [String] the title 
+    # @see #use_title_in_checksum
+    # @see #use_title_in_checksum=
+    def title=(title)
+
+    end
+
+    # Gets whether _title_ should be used when combining fails. Defaults to *false*
+    #
+    # @return [Boolean] *true* if title should be used when combining fails; *false* otherwise
+    def use_title_in_checksum
+
+    end
+
+    # Sets whether _title_ should be used when combining fails. 
+    #
+    # @param value [Boolean] *true* if title should be used when combining fails; *false* otherwise
+    def use_title_in_checksum=(value)
+
     end
   end
 end
