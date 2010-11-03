9b7ab535522ab99c3ba5207a9be7bcc7466061ea
Mikko Nylén <mikko.nylen@gmail.com>
2010-11-03 16:31:15 +0200

    Fill notification from controller

--- lib/failurous/rails/fail_notification.rb
+++ lib/failurous/rails/fail_notification.rb
@@ -1,5 +1,22 @@
 module Failurous::Rails
   class FailNotification < Failurous::FailNotification
+    def fill_from_object(object)
+      if object.kind_of?(ActionController::Base)
+        fill_from_controller(object)
+      end
+      
+      self  
+    end
     
+    def fill_from_controller(controller)
+      self.location = "#{controller.controller_name}##{controller.action_name}"   
+      request = controller.request
+      
+      self.add_field(:request, :REQUEST_METHOD, request.method, {:humanize_field_name => false}).
+        add_field(:request, :REQUEST_URI, request.request_uri, {:humanize_field_name => false}).
+        add_field(:request, :REMOTE_ADDR, request.remote_ip, {:humanize_field_name => false}).
+        add_field(:request, :HTTP_USER_AGENT, request.headers["User-Agent"], {:humanize_field_name => false}).
+        add_field(:summary, :params, controller.params)
+    end
   end
 end
