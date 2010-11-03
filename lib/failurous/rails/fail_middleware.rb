5de58aa301aa4882b79c3cc9213ea365d1ca588d
Mikko Nylén <mikko.nylen@gmail.com>
2010-11-03 16:32:03 +0200

    FailMiddleware from old failurous-rails, using the failurous-ruby's Failurous.notify

---
+++ lib/failurous/rails/fail_middleware.rb
@@ -0,0 +1,24 @@
+module Failurous::Rails
+  class FailMiddleware
+    def initialize(app)
+      @app = app
+    end
+    
+    def call(env)
+      @app.call(env)
+    rescue Exception => exception
+      unless Failurous.notifier.config.ignore_exceptions.include?(exception.class)
+        notify_of_exception(env, exception)
+      end
+      
+      raise exception
+    end
+    
+    private
+    
+      def notify_of_exception(env, exception)
+        controller = env['action_controller.instance'] || MissingController.new
+        Failurous.notify(exception, controller)
+      end
+  end
+end
