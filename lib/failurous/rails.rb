4c78b9ad6ef4ffd1178c77dcd503d6170dcc12d7
Mikko Nylén <mikko.nylen@gmail.com>
2010-11-03 16:32:19 +0200

    Own configure() method for Failurous::Rails

--- lib/failurous/rails.rb
+++ lib/failurous/rails.rb
@@ -5,4 +5,16 @@ module Failurous::Rails
   
   autoload :Config,           "#{ROOT}/rails/config"
   autoload :FailNotification, "#{ROOT}/rails/fail_notification"
+  autoload :FailMiddleware,   "#{ROOT}/rails/fail_middleware"
+  
+  def self.configure(install_middleware = true)
+    config = Config.new
+    block.call(config)
+    
+    FailNotifier.notifier = FailNotifier.new(config)
+    
+    if install_middleware
+      ::Rails.application.midleware.use FailMiddleware
+    end
+  end
 end
