4bb04e1690c068884d0d093f37202a5dab4edad3
Mikko Nylén <mikko.nylen@eficode.fi>
2010-11-05 22:34:01 +0200

    Added mocking configuration to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,8 @@ Bundler.setup
 
 require 'failurous'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'failurous', 'rails')
+
+require 'rspec'
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
