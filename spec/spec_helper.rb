f57398b1ace3282613da715395738c79d0fe4f13
Mikko Nylén <mikko.nylen@gmail.com>
2010-11-06 09:02:30 +0200

    Boot the Rails application in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ Bundler.setup
 require 'failurous'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'failurous', 'rails')
 
+require 'rack/test'
+require File.join(File.dirname(__FILE__), 'support', 'rails_app', 'config/environment')
+
 require 'rspec'
 RSpec.configure do |config|
   config.mock_with :rspec
