df60a66a609c52092b75c0b6ee464595670e04ed
Mikko Nylén <mikko.nylen@gmail.com>
2010-11-06 08:52:06 +0200

    Generated Rails 3 application for integration testing to spec/support/rails_app

---
+++ spec/support/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = 'dae71b7ffad7ff0a83932cc3146cc3f553cfa14bdf4c577cfdf4ebd57530a3d128e18759781ed9ab9fc59af976b1de056daeee4ab9b40a4fa5ecc63eeaf0454a'
