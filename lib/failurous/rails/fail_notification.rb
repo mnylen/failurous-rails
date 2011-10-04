1bff5edb5faa7b0656dc577e8930be54c9ea545d
mnylen <mikko.nylen@gmail.com>
2011-10-04 15:37:30 +0300

    Use fullpath if request_uri is not available

--- lib/failurous/rails/fail_notification.rb
+++ lib/failurous/rails/fail_notification.rb
@@ -12,8 +12,14 @@ module Failurous::Rails
       self.location = "#{controller.controller_name}##{controller.action_name}"   
       request = controller.request
       
+      request_uri = if request.respond_to?(:request_uri)
+        request.request_uri
+      else
+        request.fullpath
+      end
+      
       self.add_field(:request, :REQUEST_METHOD, request.method, {:humanize_field_name => false}).
-        add_field(:request, :REQUEST_URI, request.request_uri, {:humanize_field_name => false}).
+        add_field(:request, :REQUEST_URI, request_uri, {:humanize_field_name => false}).
         add_field(:request, :REMOTE_ADDR, request.remote_ip, {:humanize_field_name => false}).
         add_field(:request, :HTTP_USER_AGENT, request.headers["User-Agent"], {:humanize_field_name => false}).
         add_field(:summary, :params, controller.params)
